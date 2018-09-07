14a6f329fec8585f507577e104d6638d0a4c030d
Jeremy Nelson <jermnelson@gmail.com>
2018-09-07 16:31:42 -0600

    Added Jest support for eslinter
    For issue #2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
+const ERROR = 2;
+const WARN = 1;
+
 module.exports = {
-    "extends": "standard"
+    extends: "standard",
+    overrides: [
+      {
+        files: "**/*.js",
+        env: {
+          jest: true
+        },
+        plugins: [
+          "jest"
+        ],
+        rules: {
+          "jest/no-disabled-tests": "warn",
+          "jest/no-focused-tests": "error",
+          "jest/no-identical-title": "error",
+          "jest/prefer-to-have-length": "warn",
+          "jest/valid-expect": "error"
+        }
+      }
+  ]
 };
